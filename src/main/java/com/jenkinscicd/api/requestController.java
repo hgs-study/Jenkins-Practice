e6e00da26e2b13ee3b0267f774d92987b92e5308
hgs-study <hgstudy_@naver.com>
2021-08-10 18:34:24 +0900

    Initial commit

---
+++ src/main/java/com/jenkinscicd/api/requestController.java
@@ -0,0 +1,13 @@
+package com.jenkinscicd.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class requestController {
+
+    @GetMapping("/test")
+    public String test(){
+       return "test";
+    }
+}
---
+++ src/test/java/com/jenkinscicd/JenkinsCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
